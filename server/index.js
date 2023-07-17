3fff6ad1e3dd76b13167b08833363e599e8f7fb5
forrestmorrison <forrestmorrison@gmail.com>
2023-07-16 19:13:09 -0500

    protected routes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,19 @@ const weather = require("./weather");
 
 app.use(express.json());
 
-app.use(cors());
+const whiteList = ["http://127.0.0.1", "http://127.0.0.1:5500"];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin || whiteList.indexOf(origin) !== -1) {
+            callback(null, true);
+        }   else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 const limiter = rateLimit({
     windowMs: 1000,
