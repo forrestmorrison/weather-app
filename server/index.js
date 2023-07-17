bf28caf63dde208058a99ef6170d2e79d74680c9
forrestmorrison <forrestmorrison@gmail.com>
2023-07-16 19:07:14 -0500

    added rate-limit to Index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 const cors = require("cors");
 const app = express();
 const port = 3000;
@@ -9,6 +10,13 @@ const weather = require("./weather");
 app.use(express.json());
 
 app.use(cors());
+
+const limiter = rateLimit({
+    windowMs: 1000,
+    max: 1
+})
+
+app.use(limiter);
 
 // test route
 app.get("/", (req, res) => res.json({ success: "Hello World!"}));
